81aefbf860a8a483302e3f4087b97f8b99cf7c61
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2020-02-07 13:03:00 -0500

    create seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+projects = Project.create([{ title: 'Seed Project 1'}, { title: 'Seed Project 2'}])
+tasks = Task.create([{ title: 'Seed Task 1 for Project 1', project: projects.first }, { title: 'Seed Task 1 for Project 2', project: projects.last }])
