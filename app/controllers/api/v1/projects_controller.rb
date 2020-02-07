59cf9c24cd708b60472a08d8fccea550ea272268
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2020-02-07 13:59:12 -0500

    create show action in projects controller, add fastjson api gem

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,6 +1,15 @@
 class Api::V1::ProjectsController < ApplicationController
+    # GET /projects
     def index
+        #model
         @projects = Project.all
+        #response
         render json: @projects
+    end
+
+    # GET /projects/1
+    def show
+        @project = Project.find_by(id: params[:id])
+        render json: @project
     end
 end
