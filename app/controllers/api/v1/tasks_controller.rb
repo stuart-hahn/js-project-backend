5b77d23d06a3588a82f1a2c5d5e953f1e9bf1bea
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2020-02-10 21:19:12 -0500

    add index and show actions for tasks

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,2 +1,17 @@
 class Api::V1::TasksController < ApplicationController
+        # GET /tasks
+        def index
+            #model
+            @tasks = Task.all
+            serialized_data = TaskSerializer.new(@tasks)
+            #response
+            render json: serialized_data
+        end
+    
+        # GET /tasks/1
+        def show
+            @task = Task.find_by(id: params[:id])
+            serialized_data = TaskSerializer.new(@task)
+            render json: serialized_data
+        end
 end
