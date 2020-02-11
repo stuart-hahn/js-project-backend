da7a9803cc6b18dfc23bcca87fe1c483518881dc
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2020-02-10 19:59:38 -0500

    add validations and update projects controller to include serialized tasks

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -3,13 +3,15 @@ class Api::V1::ProjectsController < ApplicationController
     def index
         #model
         @projects = Project.all
+        serialized_data = ProjectSerializer.new(@projects)
         #response
-        render json: ProjectSerializer.new(@projects)
+        render json: serialized_data
     end
 
     # GET /projects/1
     def show
         @project = Project.find_by(id: params[:id])
-        render json: @project
+        serialized_data = ProjectSerializer.new(@project)
+        render json: serialized_data
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-projects = Project.create([{ title: 'Seed Project 1'}, { title: 'Seed Project 2'}])
-tasks = Task.create([{ title: 'Seed Task 1 for Project 1', project: projects.first }, { title: 'Seed Task 1 for Project 2', project: projects.last }])
+projects = Project.create([
+    { title: 'Seed Project 1'}, 
+    { title: 'Seed Project 2'}
+    ])
+tasks = Task.create([
+        { title: 'Seed Task 1 for Project 1', project: projects.first }, 
+        { title: 'Seed Task 1 for Project 2', project: projects.last }
+        ])
