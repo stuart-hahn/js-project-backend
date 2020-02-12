c25ef36dd9574cc441dd57beede7032d01dcf1a3
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2020-02-12 14:04:27 -0500

    fix tasks controller bug

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,17 +1,52 @@
 class Api::V1::TasksController < ApplicationController
-        # GET /tasks
-        def index
-            #model
-            @tasks = Task.all
-            serialized_data = TaskSerializer.new(@tasks)
-            #response
-            render json: serialized_data
-        end
-    
-        # GET /tasks/1
-        def show
-            @task = Task.find_by(id: params[:id])
-            serialized_data = TaskSerializer.new(@task)
-            render json: serialized_data
-        end
+    before_action :set_task, only: [:show, :update, :destroy]
+
+    # GET /tasks
+    def index
+      @tasks = Task.all
+  
+      render json: TaskSerializer.new(@tasks)
+    end
+  
+    # GET /tasks/1
+    def show
+      render json: TaskSerializer.new(@task)
+    end
+  
+    # POST /tasks
+    def create
+      @task = Task.new(task_params)
+  
+      if @task.save
+        render json: TaskSerializer.new(@task), status: :created
+      else
+        render json: @task.errors, status: :unprocessable_entity
+      end
+    end
+  
+    # PATCH/PUT /tasks/1
+    def update
+      if @task.update(task_params)
+        render json: TaskSerializer.new(@task)
+      else
+        render json: @task.errors, status: :unprocessable_entity
+      end
+    end
+  
+    # DELETE /tasks/1
+    def destroy
+      @task.destroy
+    end
+  
+    private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_task
+        @task = Task.find_by(id: params[:id])
+      end
+  
+      # Only allow a trusted parameter "white list" through.
+      def task_params
+        params.require(:task).permit(:title, :project_id)
+      end
 end
+
